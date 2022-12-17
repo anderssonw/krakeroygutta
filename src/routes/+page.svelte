7caa31cea33d66ce4d7a06e2090f92fe6179d020
anderssonw <wandersson.98@gmail.com>
2022-12-17 01:01:15 +0100

    add essentials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,25 @@
 <main class="contentContainer">
 	<h1>Kråkerøygutta</h1>
 	<h2>Spell og ha're gøy</h2>
+	<iframe
+		class="barisVideo"
+		playsinline
+		loop
+		title="baris"
+		width="420"
+		height="315"
+		src="https://www.youtube.com/embed/XsbCBy8X-8E"
+	/>
 </main>
 
 <style>
 	.contentContainer {
 		display: flex;
 		flex-direction: column;
-		text-align: center;
+		align-items: center;
+	}
+
+	.barisVideo {
+		border: none;
 	}
 </style>
