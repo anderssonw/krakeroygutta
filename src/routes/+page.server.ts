ffe8c007ca5c51fa3644cde1e31a89d4310f54bd
anderssonw <wandersson.98@gmail.com>
2023-08-27 22:35:35 +0200

    also add point counting using the new fantasy table
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -14,7 +14,15 @@ export const load: PageServerLoad = async ({ locals: { supabase }, parent }) => 
 			});
 		}
 
-		const { data: fantasyTeams, error: fantasyTeamsError } = await supabase.from('fantasy_teams').select().eq('season_id', season.id);
+		const { data: fantasyTeams, error: fantasyTeamsError } = await supabase
+			.from('fantasy_teams')
+			.select(
+				`
+					*,
+					fantasy_teams_players(player_id)
+				`
+			)
+			.eq('season_id', season.id);
 
 		if (fantasyTeamsError) {
 			return fail(500, {
