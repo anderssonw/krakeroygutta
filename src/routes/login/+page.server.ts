7c084b13d2467a3f52086a34de68fe24be0b35db
anderssonw <wandersson.98@gmail.com>
2023-08-28 22:03:38 +0200

    use form actions for login and register, make navbar reactive
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,25 @@
+import { fail, type Actions, redirect } from '@sveltejs/kit';
+
+export const actions = {
+	default: async ({ request, locals: { supabase } }) => {
+		const errors: Record<string, string> = {};
+		const formData = await request.formData();
+
+		const email = formData.get('email')?.toString();
+		const password = formData.get('password')?.toString();
+
+		if (email && password) {
+			const { error } = await supabase.auth.signInWithPassword({ email, password });
+
+			if (error) {
+				errors.serverError = error.message;
+				return fail(500, { errors });
+			}
+
+			throw redirect(303, '/');
+		} else {
+			errors.missing = 'Email and password must be supplied';
+			return fail(401, { errors });
+		}
+	}
+} satisfies Actions;
