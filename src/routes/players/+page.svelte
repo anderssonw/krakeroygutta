2b4e22c87cdba9d82dc17e4f5facb66d901c4e1f
anderssonw <wandersson.98@gmail.com>
2023-08-31 22:58:29 +0200

    make really good speech bubble component
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- src/routes/players/+page.svelte
+++ src/routes/players/+page.svelte
@@ -1,12 +1,25 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import Players from '$lib/components/players/Players.svelte';
+	import RuleSpeechBubble from '$lib/components/RuleSpeechBubble.svelte';
+
+	import pirateMadsSrc from '$lib/assets/piratmads.png';
 
 	// Get server data
 	export let data: PageData;
 	$: ({ mappedPlayers } = data);
+
+	const speechBubbleText: string[] = [
+		'Alle spillere er vurdert relativt til ferdighetene som befinner seg i gjengen.',
+		'Vurderingen er gjort av komiteen og trengs ikke diskuteres ytterligere.',
+		'Ferdighetene er IKKE endelige og kan endre seg fra sesong til sesong basert på egen utvikling. Innenfor en sesong er dog ferdigheter endelige'
+	];
 </script>
 
 {#if mappedPlayers}
-	<Players players={mappedPlayers} />
+	<div class="structure px-4">
+		<RuleSpeechBubble imageSrc={pirateMadsSrc} text={speechBubbleText} />
+
+		<Players players={mappedPlayers} />
+	</div>
 {/if}
