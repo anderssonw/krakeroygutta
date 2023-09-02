0ec7fed338cbf7189930b0976375d501966e2928
Magnus Gulbrandsen <magnus-kil@live.no>
2023-09-02 17:35:38 +0200

    Actions

--- src/routes/bets/+page.server.ts
+++ src/routes/bets/+page.server.ts
@@ -1,5 +1,6 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { fail, type Actions } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ locals: { supabase }, parent }) => {
 	let { season } = await parent();
@@ -25,3 +26,47 @@ export const load: PageServerLoad = async ({ locals: { supabase }, parent }) => 
         bets
     };
 };
+
+export const actions = {
+	default: async ({ request, locals: { supabase, getSession, getUser } }) => {
+		const formData = await request.formData();
+
+		const bet = formData.get('Veddemål')?.toString();
+		const value = formData.get('Sats')?.toString();
+
+		const session = await getSession();
+        const user = await getUser();
+		if (session && user && bet && value) {
+            // This is also done in fantasy, should be reworked (?)
+			let todayTimeString = new Date().toDateString();
+			const { data: season, error: seasonError } = await supabase
+				.from('seasons')
+				.select()
+				.lt('start_time', todayTimeString)
+				.gt('end_time', todayTimeString)
+				.single();
+
+			if (seasonError) {
+				return fail(500, { message: 'There is no active season. Server could not handle your request.' });
+			}
+
+            // Can use Tables<'bets'> but it contains a id
+            const betForm = {
+                user_id: user.id,
+                season_id: season.id,
+                bet: bet,
+                value: value,
+            }
+
+            const { error: error } = await supabase.from('bets').insert(betForm);
+
+			if (error) {
+				return fail(500, {
+					supabaseErrorMessage: error.message
+				});
+			}
+        }
+
+        return { success: true };
+	}
+} satisfies Actions;
--- src/routes/bets/+page.svelte
+++ src/routes/bets/+page.svelte
@@ -7,7 +7,7 @@
 	import type { PageData } from './$types';
 	export let data: PageData;
 
-	$: ({ bets } = data);
+	$: ({ user, bets } = data);
 
 	const speechBubbleText: string[] = [
 		'Her kan hver bruker legge ut 1 veddemål relatert til årets sesong.',
@@ -15,29 +15,39 @@
 		'Betalingen skjer 1-til-1 og er bundet av kjærlighet til hverandre.'
 	];
 
+    function betExists() {
+        return (bets?.filter(b => b.user_id == user?.id).length > 0) ? true : false;
+    }
+    function myBet(bet_id: string, user_id: string) {
+        return bet_id == user_id;
+    }
+
     let bet: string;
     let value: string;
 </script>
 
 <div class="structure">
-    <button on:click={() => console.log(bets)}>CLICK</button>
-
     <h1> Bets </h1>
     <RuleSpeechBubble imageSrc={pirateMadsSrc} text={speechBubbleText} mirror={true} />
 
-    <h3> Opprett ett veddemål </h3>
-    <form class="form">
-		<div class="form-structure">
-            <TextField label="Veddemål" placeholder="Jeg vedder .." type="text" value={bet} />
-            <TextField label="Sats" placeholder="50" type="number" value={value} />
-        </div>
-    </form>
+    {#if !betExists()}
+        <h3> Opprett ett veddemål </h3>
+        <form class="form" method="POST">
+            <div class="form-structure">
+                <TextField label="Veddemål" placeholder="Jeg vedder .." type="text" value={bet} />
+                <TextField label="Sats" placeholder="50" type="number" value={value} />
+                <div>
+                    <input type="submit" class="btn" value={'Send inn'} />
+                </div>
+            </div>
+        </form>
+    {/if}
 
     <h3> Eksisterende veddemål </h3>
     <div class="w-full flex flex-col">
         {#each bets as bet}
             <div class="flex flex-row">
-                <div class="w-5/12 bg-secondary-color-light p-6 text-primary-color-dark rounded-tl-3xl rounded-tr-3xl rounded-br-3xl">
+                <div class="w-5/12 bet">
                     Veddemål: 				
                     <ul class="list-disc list-inside pl-4">
                         <li> {bet.bet} </li>
