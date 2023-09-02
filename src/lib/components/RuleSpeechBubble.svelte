f46b29128398bea40670b844ee914bfd2bbdf1b5
Magnus Gulbrandsen <magnus-kil@live.no>
2023-09-02 11:58:04 +0200

    speech on mobile + correct color

--- src/lib/components/RuleSpeechBubble.svelte
+++ src/lib/components/RuleSpeechBubble.svelte
@@ -17,15 +17,15 @@
 	const mirroredTriangle = mirror ? '-scale-x-100' : '';
 </script>
 
-<div class="max-w-[50ch]">
-	<div class="bg-slate-100 rounded-3xl grid p-8 gap-2">
+<div class="w-[70%] tablet:max-w-[50ch]">
+	<div class="bg-secondary-color-light rounded-3xl grid p-8 gap-2">
 		{#each paragraphs as paragraph}
 			<p class="text-primary-color-dark">{paragraph}</p>
 		{/each}
 	</div>
 	<div class={`flex ${translateDirection}`}>
 		<img class="-translate-y-6 max-w-[150px] justify-" src={imageSrc} alt="En spelare" />
-		<div class={`speech-triangle bg-slate-100 h-12 w-12  ${mirroredTriangle}`} />
+		<div class={`speech-triangle bg-secondary-color-light h-12 w-12  ${mirroredTriangle}`} />
 	</div>
 </div>
 
---
+++ src/routes/bets/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import Players from '$lib/components/players/Players.svelte';
+	import RuleSpeechBubble from '$lib/components/RuleSpeechBubble.svelte';
+
+	import pirateMadsSrc from '$lib/assets/piratmads.png';
+
+	const speechBubbleText: string[] = [
+		'Her kan hver bruker legge ut 1 veddemål relatert til årets sesong.',
+		'De andre brukerne kan velge å vedde imot, og valget låses dagen fantasy sesongen starter.',
+		'Betalingen skjer 1-til-1 og er bundet av kjærlighet til hverandre.'
+	];
+</script>
+
+<div class="structure">
+    <h1> Bets </h1>
+    <RuleSpeechBubble imageSrc={pirateMadsSrc} text={speechBubbleText} mirror={true} />
+</div>
