37d8aba80fc0866963774a9cfb6bc60eb71282a0
Magnus Gulbrandsen <magnus-kil@live.no>
2023-09-03 11:16:33 +0200

    Updated buttons

---
+++ src/lib/components/index/SeasonButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+
+    export let image: string;
+    export let route: string;
+</script>
+
+<div 
+    class="w-10/12 tablet:w-5/12 flex flex-col items-center grow group 
+    hover:cursor-pointer hover:scale-105 active:scale-100  border-4 border-secondary-color-light rounded-3xl" on:mouseup={() => goto(`/${route.toLowerCase()}`)}>
+
+    <img src="/index/{image}" alt="players" class="w-[80%] mb-2 group-hover:scale-105 group-active:scale-100" />
+    <h1 class="mb-4 group-hover:scale-105 group-active:scale-100">{route}</h1>
+</div>
