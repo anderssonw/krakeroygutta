3b131d06556eaed443396e9ec16d575a1d037f7f
anderssonw <wandersson.98@gmail.com>
2023-09-12 21:39:34 +0200

    cooler page
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import RuleSpeechBubble from '$lib/components/common/RuleSpeechBubble.svelte';
+	import pirateMadsSrc from '$lib/assets/piratmads.png';
 </script>
 
-<div class="structure">
-	<h1>Det ser ut som noe har gått galt med siden!</h1>
+<div class="structure text-center">
+	<h1>Det ser ut som noe har gått galt!</h1>
 
-	<h2>
-		Oss stakkars utviklere av siden kan alltids gjøre feil, så for å hjelpe oss gjøre siden bedre kan du ta skjermbilde av meldingen under:
-	</h2>
+	<h2>Feil kan alltid skje, så for å hjelpe oss gjøre siden bedre kan du ta skjermbilde av meldingen under Mads</h2>
 
-	<h3>{$page.error?.devHelper}</h3>
+	<RuleSpeechBubble
+		mirror
+		imageSrc={pirateMadsSrc}
+		text="Husk å være snill med Magnus og William, selv om ikke alt alltid går på skinner. Det kan være vanskelig å lage gode nettsider!"
+	/>
 
-	<h3>{$page.error?.message}</h3>
+	<h4>{$page.error?.devHelper}</h4>
+
+	<p>{$page.error?.message}</p>
 </div>
