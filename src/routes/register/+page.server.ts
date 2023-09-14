cf02374dec025a5caa804ef3579d00032920dc62
anderssonw <wandersson.98@gmail.com>
2023-09-14 20:54:15 +0200

    add nickname to user registration

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,30 +1,56 @@
-import { fail, type Actions, redirect } from '@sveltejs/kit';
+import { fail, type Actions, redirect, error } from '@sveltejs/kit';
 
 export const actions = {
 	default: async ({ request, locals: { supabase } }) => {
 		const formData = await request.formData();
 		const email = formData.get('email')?.toString();
 		const password = formData.get('password')?.toString();
-
-		/* TODO: Use nickname */
 		const nickname = formData.get('nickname')?.toString();
 
-		if (email && password) {
-			const { error: error } = await supabase.auth.signUp({
-				email,
-				password
-			});
-			console.log(error);
+		let formHints: string[] = [];
 
-			if (error) {
-				return fail(500, {
-					supabaseErrorMessage: error.message
+		if (!email || email === '') {
+			formHints.push('Email may not be empty');
+		}
+
+		let passwordIsGood = false;
+
+		if (!password || password === '') {
+			formHints.push('Password may not be empty');
+		} else if (password.length < 8) {
+			formHints.push('Password must be at least 8 characters');
+		} else {
+			passwordIsGood = true;
+		}
+
+		if (!nickname || nickname === '') {
+			formHints.push('Nickname may not be empty');
+		}
+
+		if (email && password && passwordIsGood && nickname) {
+			const { error: authError } = await supabase.auth.signUp({
+				email,
+				password,
+				options: {
+					data: {
+						nickname
+					}
+				}
+			});
+
+			if (authError) {
+				console.log(authError);
+				throw error(500, {
+					message: authError.message,
+					devHelper: '/register'
 				});
 			}
 
 			throw redirect(303, '/');
 		}
 
-		return fail(400);
+		return fail(400, {
+			formHints
+		});
 	}
 } satisfies Actions;
