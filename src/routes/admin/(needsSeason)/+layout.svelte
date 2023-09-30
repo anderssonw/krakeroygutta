d5c577cb019dd4fca5518b65a63d1d2535d2ba64
anderssonw <wandersson.98@gmail.com>
2023-09-30 14:01:32 +0200

    adminpage for matches wip
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

---
+++ src/routes/admin/(needsSeason)/+layout.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import DropdownMenu from '$lib/components/admin/dropdownMenu.svelte';
+	import type { DropdownOption } from '$lib/types/newTypes';
+	import type { LayoutData } from './$types';
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+
+	export let data: LayoutData;
+	$: ({ seasons } = data);
+	$: seasonOption = null as DropdownOption | null;
+	$: setSeasonURL(seasonOption);
+	onMount(() => {
+		let seasonParamId = Number($page.url.searchParams.get('season'));
+		if (seasonParamId) {
+			let seasonFromParam = seasons?.find((season) => season.id == seasonParamId);
+			if (!seasonFromParam) return;
+			seasonOption = {
+				id: seasonFromParam.id,
+				name: seasonFromParam.name
+			};
+		}
+	});
+	const setSeasonURL = (seasonOption: DropdownOption | null) => {
+		if (!browser) return;
+
+		let url = $page.url;
+		if (seasonOption) {
+			let query = new URLSearchParams(url.searchParams.toString());
+			query.set('season', `${seasonOption.id}`);
+			goto(`?${query.toString()}`);
+		} else {
+			goto(`${url.origin}${url.pathname}`);
+		}
+	};
+	const getSeasonOptions = () => {
+		if (!seasons) return [];
+		return seasons?.map((season) => {
+			return {
+				name: season.name,
+				id: season.id
+			} satisfies DropdownOption;
+		});
+	};
+</script>
+
+<div class="structure">
+	<div class="w-3/5">
+		<DropdownMenu header={'Velg Sesong'} option={'sesong'} options={getSeasonOptions()} bind:selectedOption={seasonOption} />
+	</div>
+	{#if seasonOption}
+		<slot />
+	{/if}
+</div>
