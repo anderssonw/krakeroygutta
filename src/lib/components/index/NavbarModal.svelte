212e5d3547a4fbce82bf31c6e884f4ef0e7148ae
anderssonw <wandersson.98@gmail.com>
2023-10-15 22:40:24 +0200

    slight improvement navbar
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- src/lib/components/index/NavbarModal.svelte
+++ src/lib/components/index/NavbarModal.svelte
@@ -1,24 +1,22 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import type { Route } from '$lib/shared/routes';
+	import ArrowRightIcon from 'virtual:icons/ph/arrow-right';
 
-    export let routes: Route[];
+	export let routes: Route[];
 	export let showMobileNavbar: boolean;
 
-    $: navbarAnimation = `transition-all duration-500 ${showMobileNavbar ? 'translate-x-0' : '-translate-x-full'}`;
+	$: navbarAnimation = `${showMobileNavbar ? 'translate-x-0' : 'translate-x-full'}`;
 </script>
 
-
-<div class="fixed bottom-0 top-0 right-0 left-0 z-50 bg-secondary-color-dark {navbarAnimation}">
-    <div class="text-primary-color-dark absolute top-10 right-10 hover:cursor-pointer" on:mouseup={() => showMobileNavbar = false}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-        </svg>
-    </div>
-
-    <div class="structure">
-        {#each routes as route}
-            <a href="{route.route}" on:mouseup={() => showMobileNavbar = false}> <h1 class="navbtn text-primary-color-dark">{route.name}</h1> </a>
-        {/each}
-    </div>
+<div class="flex flex-col fixed bottom-0 top-0 right-0 w-3/4 z-50 bg-secondary-color-dark transition-all duration-500 {navbarAnimation}">
+	<button type="button" class="self-end text-black p-6 hover:cursor-pointer" on:click={() => (showMobileNavbar = false)}>
+		<ArrowRightIcon class="text-2xl" />
+	</button>
+	<div class="flex flex-col">
+		{#each routes as route}
+			<a class="text-center px-4 py-0 my-3 text-black text-lg" href={route.route} on:mouseup={() => (showMobileNavbar = false)}>
+				{route.name}
+			</a>
+		{/each}
+	</div>
 </div>
