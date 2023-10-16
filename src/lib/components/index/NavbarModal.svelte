a6be2737b7568f7ed27d94d31097b794c630953f
anderssonw <wandersson.98@gmail.com>
2023-10-16 10:43:41 +0200

    prettier navbar
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- src/lib/components/index/NavbarModal.svelte
+++ src/lib/components/index/NavbarModal.svelte
@@ -8,13 +8,13 @@
 	$: navbarAnimation = `${showMobileNavbar ? 'translate-x-0' : 'translate-x-full'}`;
 </script>
 
-<div class="flex flex-col fixed bottom-0 top-0 right-0 w-3/4 z-50 bg-secondary-color-dark transition-all duration-500 {navbarAnimation}">
-	<button type="button" class="self-end text-black p-6 hover:cursor-pointer" on:click={() => (showMobileNavbar = false)}>
+<div class="flex flex-col fixed bottom-0 top-0 right-0 w-11/12 z-50 bg-team-black transition-all duration-500 {navbarAnimation}">
+	<button type="button" class="self-end p-6 hover:cursor-pointer" on:click={() => (showMobileNavbar = false)}>
 		<ArrowRightIcon class="text-2xl" />
 	</button>
 	<div class="flex flex-col">
 		{#each routes as route}
-			<a class="text-center px-4 py-0 my-3 text-black text-lg" href={route.route} on:mouseup={() => (showMobileNavbar = false)}>
+			<a class="text-center px-4 py-0 my-3 text-lg" href={route.route} on:mouseup={() => (showMobileNavbar = false)}>
 				{route.name}
 			</a>
 		{/each}
