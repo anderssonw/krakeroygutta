c4d2f67c404f6efb3ae082dd7fff35962c2a9f2b
anderssonw <wandersson.98@gmail.com>
2023-10-31 22:18:08 +0100

    add some shitty tooltips
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- src/lib/components/fantasy/FantasyCardButtons.svelte
+++ src/lib/components/fantasy/FantasyCardButtons.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import type { FantasyForm, FullPlayer } from "$lib/types/newTypes";
-    import DeleteIcon from 'virtual:icons/material-symbols/delete-outline';
-    import NotCaptainIcon from 'virtual:icons/ic/outline-panorama-photosphere';
-    import CaptainIcon from 'virtual:icons/ic/outline-panorama-photosphere-select';
-    import UserIcon from 'virtual:icons/ph/user-circle';
+	import type { FantasyForm, FullPlayer } from '$lib/types/newTypes';
+	import DeleteIcon from 'virtual:icons/material-symbols/delete-outline';
+	import NotCaptainIcon from 'virtual:icons/ic/outline-panorama-photosphere';
+	import CaptainIcon from 'virtual:icons/ic/outline-panorama-photosphere-select';
+	import UserIcon from 'virtual:icons/ph/user-circle';
 
-    export let player: FullPlayer | null;
+	export let player: FullPlayer | null;
 	export let fantasyForm: FantasyForm;
 	export let position: number;
 
-    function handleCaptainClick(playerId: number) {
+	function handleCaptainClick(playerId: number) {
 		fantasyForm.captainId = playerId;
 	}
 
@@ -23,19 +23,18 @@
 	$: isCaptain = fantasyForm.captainId == player?.player_id;
 </script>
 
-<button type="button" class="btn-icon" on:click={() => sellPlayer()}>
-    <DeleteIcon font-size="large" />                                               
+<button type="button" class="btn-icon" title="Selg Spiller" on:click={() => sellPlayer()}>
+	<DeleteIcon font-size="large" />
 </button>
-<button type="button" class="btn-icon" on:click={() => handleCaptainClick(player?.player_id ?? -1)}>
-    {#if isCaptain}
-        <CaptainIcon font-size="large" />
-    {:else}
-        <NotCaptainIcon font-size="large" />
-    {/if}
-    
+<button type="button" title="Velg som Kaptein" class="btn-icon" on:click={() => handleCaptainClick(player?.player_id ?? -1)}>
+	{#if isCaptain}
+		<CaptainIcon font-size="large" />
+	{:else}
+		<NotCaptainIcon font-size="large" />
+	{/if}
 </button>
 <a href="/players/{player?.player_id}" target="_blank" rel="noreferrer" class="p-0">
-    <button type="button" class="btn-icon">
-        <UserIcon font-size="large" />                  
-    </button>
+	<button type="button" title="Se Detaljert Spillerstatistikk" class="btn-icon">
+		<UserIcon font-size="large" />
+	</button>
 </a>
