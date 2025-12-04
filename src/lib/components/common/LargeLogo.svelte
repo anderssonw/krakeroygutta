fe92d02cc29624999f3e6124336ffae753ad4143
Magnus Gulbrandsen <magnus-kil@live.no>
2025-12-04 18:29:09 +0100

    Lowres option

--- src/lib/components/common/LargeLogo.svelte
+++ src/lib/components/common/LargeLogo.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	import headerLarge from '$lib/assets/headerLarge.png';
 	import headerLargeDisco from '$lib/assets/headerLargeDisco.webp';
+	import headerLargeDiscoLowres from '$lib/assets/headerLargeDiscoLowres.webp';
 </script>
 
-<div class="w-[90%] sm:w-[80%]">
+<div class="px-4 block tablet:hidden">
+	<picture>
+		<source srcset={headerLargeDiscoLowres} type="image/webp" />
+		<img src={headerLarge} alt="logo" />
+	</picture>
+</div>
+
+<div class="w-[90%] sm:w-[80%] hidden tablet:block">
 	<picture>
 		<source srcset={headerLargeDisco} type="image/webp" />
 		<img src={headerLarge} alt="logo" />
